461cf5722d862ef7541713baece352ee8267a3c5
Curtis Nguyen <hieunv2406@gmail.com>
2022-07-05 00:12:31 +0700

    init

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../Asserts/logo.svg';
+import '../Styleds/App.css';
 
 function App() {
   return (
